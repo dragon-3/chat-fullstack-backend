47bd2c0808656d24ed4f0402180596c558496aae
Sean Narron <seannarron9@gmail.com>
2022-10-07 17:12:30 -0700

    Modified.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const cors = require('cors');
-const socketio = require('socket.io');
+const {Server} = require('socket.io');
 
 const router = require('./router');
 
@@ -12,7 +12,12 @@ app.use(cors());
 
 const server = http.createServer(app);
 
-const io = socketio(server)
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:3000',
+        methods: ["GET", "POST"]
+    }
+})
 
 io.on("connection", (socket) => {
     console.log(`User has connected with id: ${socket.id}`)
